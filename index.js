0ff90a9865da6b5c64b8b54956f9da3b2c7fdafd
Theo Booth <theorlbooth@googlemail.com>
2020-03-03 16:37:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function interativeLog(array) {
+  array.forEach()
+    console.log(`${index}:${element}`)
+}
+
+function iterate(callback) {
+an_array = ["milk", "cat", "pop"]
+    an_array.forEach(callback)
+    return an_array
+}
+
