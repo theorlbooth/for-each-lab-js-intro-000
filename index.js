bdf72221b61924732f00027bcc5dec677e10f26c
Theo Booth <theorlbooth@googlemail.com>
2020-03-03 16:33:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function interativeLog(array) {
   array.forEach()
     console.log(`${index}:${element}`)
 }
+
+function iterate(callback) {
+an_array = ["milk", "cat", "pop"]
+    an_array.forEach(callback)
+    return an_array
+}
+
